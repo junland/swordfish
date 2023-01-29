4d159a9abe5a8ecd19887f8b8d6b84dc302b6a66
John Unland <unlandj2012@gmail.com>
2023-01-29 16:50:12 -0600

    scripts: update script for patches

--- scripts/apply-patches.py
+++ scripts/apply-patches.py
@@ -6,8 +6,10 @@ Import("env") # type: ignore
 project_dir = env["PROJECT_DIR"] # type: ignore
 workspace_dir = env["PROJECT_DIR"] # type: ignore
 src_dir = env["PROJECT_SRC_DIR"] # type: ignore
+build_dir = env["PROJECT_BUILD_DIR"] # type: ignore
 
 # Output project_dir to console
 print("Project dir: " + project_dir)
 print("Workspace dir: " + workspace_dir)
 print("Source dir: " + src_dir)
+print("Build dir: " + build_dir)
