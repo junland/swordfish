c59ae166a74be0ad14cf2a701692b09a4ffbc177
John Unland <unlandj2012@gmail.com>
2023-04-05 13:23:29 -0500

    config: Change S1 default bed size

--- config/creality_s1_plus_junland_default/Configuration.h
+++ config/creality_s1_plus_junland_default/Configuration.h
@@ -135,7 +135,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 3 S1 - ABL F4"
+#define CUSTOM_MACHINE_NAME "Ender 3 S1 Plus - ABL F4"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1791,8 +1791,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 300
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1800,7 +1800,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 275
+#define Z_MAX_POS 300
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
